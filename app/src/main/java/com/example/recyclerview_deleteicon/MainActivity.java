b4e1babe94aab5a4abf0a8d76fd162ad3a9f801a
Md. Rezwanul Islam <rezwan.sec@gmail.com>
2020-09-21 11:44:08 +0600

    Deleteicon added

--- app/src/main/java/com/example/recyclerview_deleteicon/MainActivity.java
+++ app/src/main/java/com/example/recyclerview_deleteicon/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.recyclerview_deleteicon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<ExampleItem> mExampleList;
+
+    private RecyclerView mRecyclerView;
+    private ExampleAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createExampleList();
+        buildRecyclerView();
+
     }
+
+    public void removeItem(int position) {
+        mExampleList.remove(position);
+        mAdapter.notifyItemRemoved(position);
+        //  mAdapter.notifyDataSetChanged();
+    }
+
+    public void createExampleList(){
+
+        mExampleList = new ArrayList<>();
+        mExampleList.add(new ExampleItem(R.drawable.ic_android, "Line 1", "Line 2"));
+        mExampleList.add(new ExampleItem(R.drawable.ic_baseline, "Line 3", "Line 4"));
+        mExampleList.add(new ExampleItem(R.drawable.ic_archive, "Line 5", "Line 6"));
+
+
+    }
+    public void buildRecyclerView() {
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(mExampleList);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListener(new ExampleAdapter.OnItemClickListener() {
+            @Override
+            public void onDeleteClick(int position) {
+                removeItem(position);
+            }
+        });
+    }
+
 }
